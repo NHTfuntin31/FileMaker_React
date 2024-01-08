0d57c2e9d6c30209411cb10d9ecff961c6868800
フン　ティン <funtin3110@gmail.com>
2024-01-09 00:53:38 +0900

    inosuke

--- FileMaker_React/src/App.tsx
+++ FileMaker_React/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Login from "./pages/login"
 import MyPage from "./pages/mypage"
+import Testzero from "./pages/TestPage_01"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
         <Route path="/" element={<Login />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/testzero" element={<Testzero />} />
       </Routes>
     </Router>
   )
