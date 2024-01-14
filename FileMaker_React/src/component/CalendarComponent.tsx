fe012f04200f59c52f41f53f7e19ad62cade952c
フン　ティン <funtin3110@gmail.com>
2024-01-15 00:07:40 +0900

    inosuke

--- FileMaker_React/src/component/CalendarComponent.tsx
+++ FileMaker_React/src/component/CalendarComponent.tsx
@@ -111,11 +111,12 @@ const getData = (yy: number, mm: number, startOnMonday: boolean) => {
 	//何曜日かを確認
 	//1 2 3 4 5 6 0
 	let firstWeek = first.getDay();
+	const firstWeek_pro = firstWeek
 
 
 	//30 31 29
 	const lastDate = last.getDate();
-	const lastDate_demo = last_demo.getDate();
+	const lastDate_m = last_demo.getDate();
 	// console.log(lastDate_demo);
 
 
@@ -133,14 +134,18 @@ const getData = (yy: number, mm: number, startOnMonday: boolean) => {
 	}
 
 	for (let i = 1; i <= lastDate; i += 1) {
+		if((firstWeek_pro-i) >= 0) {
+			weekArray[(firstWeek_pro-i)] = String(lastDate_m -(i-1))
+		}
 		weekArray[firstWeek] = String(i);
-		console.log(firstWeek);
+
 		if (firstWeek === 6 || i === lastDate) {
 			result.push(weekArray);
 
 			weekArray = init();
 			firstWeek = -1;
 		}
+
 		firstWeek += 1;
 	}
 	return result;
@@ -156,8 +161,23 @@ const getCalendar = (year: number, month: number, startOnMonday: boolean) => {
 		yy -= 1;
 		mm = 12;
 	}
+
+	let yy_m = (mm - 1) == 0 ? yy - 1 : yy
+	let yy_p = (mm + 1) == 13 ? yy + 1 : yy
+	let mm_m = (mm - 1) == 0 ? 12 : mm - 1
+	let mm_p = (mm + 1) == 13 ? 1 : mm + 1
+
+	const calendar_m = getData(yy_m, mm_m, startOnMonday);
 	const calendar = getData(yy, mm, startOnMonday);
-	const result = { year: yy, month: mm, calendar };
+	const calendar_p = getData(yy_p, mm_p, startOnMonday);
+	const result = [
+		{ year: yy_m, month: mm_m, calendar: calendar_m },
+		{ year: yy, month: mm, calendar: calendar },
+		{ year: yy_p, month: mm_p, calendar: calendar_p },
+	];
+	const result1 = { year: yy, month: mm, calendar };
+
+	console.log(result)
 
 	return result;
 };
@@ -210,63 +230,60 @@ const WeekRow = ({
 };
 
 const Calendar = (props: any) => {
-	const { year, month, schedules, onClick, startOnMonday } = props;
-	const data_m = getCalendar(year, month - 1, startOnMonday);
+	const { year, month, schedules, onClick, startOnMonday } = props; 
 	const data = getCalendar(year, month, startOnMonday);
-	const data_p = getCalendar(year, month + 1, startOnMonday);
-	
-	const dataArr = [data_m, data, data_p]
-
+	console.log(data)
 	return (
 		<div className="flex gap-20">
-			{dataArr.map((item: any, index: number) => (
+			{data.map((item: any, index: number) => (
 				<div className="flex h-[100%] w-[100%] flex-1 flex-col" key={index}>
 					<WeekHeader startOnMonday={startOnMonday} />
 					{item.calendar.map((week: string[], key: number) => {
-					return (
-						<WeekRow
-							className={cn(
-								key === data?.calendar.length - 1 ? "border-b-0" : "",
-							)}
-							key={key}
-						>
-							{week.map((e, _key) => {
-								return (
-									<div
-										key={`day-${_key}`}
-										className={cn(
-											`flex flex-1 flex-col border-r border-gray-100 py-2 text-sm`,
-											e === ""
-												? "bg-orange-400 opacity-15"
-												: "",
-										)}
-										onClick={() => onClick(e)}
-									>
-										{ 
-											(key == 0 && e == "") ? (<span>wtf</span>)
-											:(key > 1 && e == "") ? (<span>dcm</span>)
-											: (<span>{e}</span>)
-										}
-										<div className="mt-1 flex justify-center gap-[2px]">
-											{schedules?.map((s: any, key: number) => {
-												if (
-													e !== ""
-												) {
-													return (
-														<ScheduleItem
-															key={`${s.year}${s.month}${s.day}${key}`}
-															color={s.color}
-														/>
-													);
-												}
-											})}
+						return (
+							<WeekRow
+								className={cn(
+									// key === data?.calendar.length - 1 ? "border-b-0" : "",
+									
+								)}
+								key={key}
+							>
+								{week.map((e, _key) => {
+									return (
+										<div
+											key={`day-${_key}`}
+											className={cn(
+												`flex flex-1 flex-col border-r border-gray-100 py-2 text-sm`,
+												e === ""
+													? "bg-orange-400 opacity-15"
+													: "",
+											)}
+											onClick={() => onClick(e)}
+										>
+											{
+												(key == 0 && e == "") ? (<span>wtf</span>)
+													: (key > 1 && e == "") ? (<span>dcm</span>)
+														: (<span>{e}</span>)
+											}
+											<div className="mt-1 flex justify-center gap-[2px]">
+												{schedules?.map((s: any, key: number) => {
+													if (
+														e !== ""
+													) {
+														return (
+															<ScheduleItem
+																key={`${s.year}${s.month}${s.day}${key}`}
+																color={s.color}
+															/>
+														);
+													}
+												})}
+											</div>
 										</div>
-									</div>
-								);
-							})}
-						</WeekRow>
-					);
-				})}
+									);
+								})}
+							</WeekRow>
+						);
+					})}
 				</div>
 			))}
 		</div>
